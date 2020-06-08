c6be2edfae9f515885666c650f5caf72d1cb0cf1
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 15:35:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,10 +2,18 @@
 
   function initBot(){
     var teams = $("div.ipe-SoccerGridContainer").find("div.ipe-SoccerGridColumn div.ipe-SoccerGridCell");
-    var team1 = teams.eq(0).text();
-    var team2 = teams.eq(0).text();
     
-    alert(team1+" vs "+team2);
+    if(teams.length > 0){
+      
+      var team1 = teams.eq(0).text();
+      var team2 = teams.eq(1).text();
+    
+      alert(team1+" vs "+team2);
+      
+    }else{
+      alert("Entre em jogo de futebol para iniciar o bot.");
+    }
+    
   }
 
   if(typeof jQuery == 'undefined'){
