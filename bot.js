cdfe629e8734bc0ae661dc31ef23782693f00cd9
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 13:33:56 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,7 @@
-alert("ok");
+if(!jQuery){
+  var e = document.createElement('script');
+  e.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+  document.getElementsByTagName('body')[0].appendChild(e);
+}
+
+$("body").css("opacity", 0.4);
