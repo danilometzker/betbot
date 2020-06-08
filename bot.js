398e3d5da08d8817c26529b2fc958492808c14da
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 16:32:39 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,46 +7,41 @@
       
       var team1 = teams.eq(0).text();
       var team2 = teams.eq(1).text();
-    
-      toastr.success("<br>"+team1+" vs "+team2, 'BetBot: Jogo localizado', {
-        tapToDismiss: true,
-        showMethod: 'fadeIn',
-        showDuration: 300,
-        progressBar: true,
-        positionClass: 'toast-top-center',
+
+      swal({
+        title: "Jogo localizado",
+        text: team1+" vs "+team2,
+        icon: "success",
       });
       
     }else{
-      toastr.error("<br>O BetBot só pode ser inicializado em uma página de jogo de futebol.", 'BetBot: Erro', {
-        tapToDismiss: true,
-        showMethod: 'fadeIn',
-        showDuration: 300,
-        progressBar: true,
-        positionClass: 'toast-top-center',
+      swal({
+        title: "Erro",
+        text: "O BetBot só pode ser inicializado em uma página de jogo de futebol.",
+        icon: "error",
       });
+      
     }
     
   }
 
-  function loadToastr(){
+  function loadSwal(){
     var script = document.createElement('script');
-    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js';
+    script.src = 'https://unpkg.com/sweetalert/dist/sweetalert.min.js';
     script.onload = initBot;
     virtualdoc.body.appendChild(script);
-    
-    var link = document.createElement('link');
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    link.href = 'https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css';
-    virtualdoc.body.appendChild(link);
+     
+    var style = document.createElement('style');
+    style.innerHTML(".swal-modal {font-family:sans-serif; }");
+    virtualdoc.body.appendChild(style);
   }
 
   if(typeof jQuery == 'undefined'){
     var script = document.createElement('script');
     script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js';
-    script.onload = loadToastr;
+    script.onload = loadSwal;
     virtualdoc.body.appendChild(script);
   }else{
-    loadToastr();
+    loadSwal();
   }
 })(document)
