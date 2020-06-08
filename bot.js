08f223c537fc3e8bd738472744a1099315d8cb4d
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 13:48:50 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,11 @@
+var $ = false;
 if(typeof jQuery == "function"){
   var e = document.createElement('script');
   e.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js";
+  e.type = 'text/javascript';
+  e.onload = function() {
+    var $ = window.jQuery;
+  };
   document.getElementsByTagName('body')[0].appendChild(e);
 }
 
