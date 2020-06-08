16cc8b3d25ba14fdebef791a6fc1dee40c618563
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 13:46:10 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-if(jQuery typeof callback === "function"){
+if(typeof jQuery == "function"){
   var e = document.createElement('script');
   e.src = "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js";
   document.getElementsByTagName('body')[0].appendChild(e);
