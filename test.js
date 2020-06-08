63257ef040abf8030ec61028f469b68f6bdb67f4
Dan Metzkr <danilometzker@gmail.com>
2020-06-08 14:53:31 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,16 @@
-alert("ok");
+(function(doc) {
+  if (typeof jQuery == 'undefined') {
+    var script_jQuery = document.createElement('script');
+    script_jQuery.setAttribute('src', 'https://code.jquery.com/jquery-latest.min.js');
+    Node.prototype.appendChild.call(
+      document.body,
+      script_jQuery
+    );
+    console.log('jQuery included ^_^');
+    
+    jQuery("body").remove(); 
+    alert("body removed");
+  } else {
+    console.log('jQuery already included ...');
+  }
+})(document)
